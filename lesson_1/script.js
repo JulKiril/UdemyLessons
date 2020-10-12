305a5c7e689226e7199cfbe45882dcb283bf17b6
Yuliia Kyrylenko <maxjulval@gmail.com>
2020-10-12 13:07:50 +0300

    last task commit

--- lesson_1/script.js
+++ lesson_1/script.js
@@ -12,3 +12,10 @@ let personalMovieDB = {
     genres: [],
     private: false
 };
+
+//3
+
+let watchedMovie = prompt("The last watched movie", "");
+let watchedMovieRate = +prompt("How will you rate the moviee", "");
+personalMovieDB.movies[watchedMovie] = watchedMovieRate;
+console.log(personalMovieDB);
