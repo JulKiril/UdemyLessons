57c02b713c584b6a038c285ffc610e2e0d5dfce3
Yuliia Kyrylenko <maxjulval@gmail.com>
2020-10-12 17:58:08 +0300

    second practice part2

--- practice_2_loop/script.js
+++ practice_2_loop/script.js
@@ -1,5 +1,20 @@
 "use strict";
 let numberOfFilms = +prompt("How many films did you watch?", "");
+
+//3
+
+if(numberOfFilms < 10){
+    alert("Ups, too little films DB");
+}
+else if(numberOfFilms >= 10 && numberOfFilms <= 30){
+    alert("You are a classic watcher");
+}
+else if(numberOfFilms > 30){
+    alert("You are a superwatcher");
+}
+else{
+    alert("Something was wrong");
+}
 let personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -8,10 +23,22 @@ let personalMovieDB = {
     private: false
 };
 
-//1
+//1, 2
 
 for(let i=0; i<2; i++){
-    let watchedMovie = prompt("The last watched movie", ""),
-        watchedMovieRate = +prompt("How will you rate the movie", "");
-        personalMovieDB.movies[watchedMovie] = watchedMovieRate;
+    let a = prompt("The last watched movie", ""),
+        b = +prompt("How will you rate the movie", "");
+
+        if (a != "" && b != "" && a != null && b != null && a.length < 50)
+        {
+            console.log("Good!");
+            personalMovieDB.movies[a] = b;
+        }
+        else {    
+            console.log("Error!");
+            i--;
+        }
 }
+
+
+
