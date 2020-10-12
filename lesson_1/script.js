b274c1996aa8df5a482e11c8d8f32c0880c55989
Yuliia Kyrylenko <maxjulval@gmail.com>
2020-10-12 13:10:58 +0300

    last task commit

--- lesson_1/script.js
+++ lesson_1/script.js
@@ -15,7 +15,10 @@ let personalMovieDB = {
 
 //3
 
-let watchedMovie = prompt("The last watched movie", "");
-let watchedMovieRate = +prompt("How will you rate the moviee", "");
+let watchedMovie = prompt("The last watched movie", ""),
+    watchedMovieRate = +prompt("How will you rate the moviee", ""),
+    c = prompt("The last watched movie", ""),
+    d = +prompt("How will you rate the moviee", "");
 personalMovieDB.movies[watchedMovie] = watchedMovieRate;
+personalMovieDB.movies[c] = d;
 console.log(personalMovieDB);
