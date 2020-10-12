d6b5aa712aa25a478f576d91aa16f446892c1467
Yuliia Kyrylenko <maxjulval@gmail.com>
2020-10-12 12:50:14 +0300

    second task commit

--- lesson_1/script.js
+++ lesson_1/script.js
@@ -1,2 +1,14 @@
 "use strict";
+//1)
+
 let numberOfFilms = +prompt("How many films did you watch?", "");
+
+//2
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
