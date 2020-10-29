609eb1df2472dbf714f848940adbab4ae815489b
Yuliia Kyrylenko <maxjulval@gmail.com>
2020-10-29 17:15:56 +0200

    updated request answer

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -99,16 +99,17 @@ function setClock(selector,endTime){
     ///////////MODAL WINDOW
 
     const modalBtn = document.querySelectorAll('[data-modal]'),
-            modalClose = document.querySelector('[data-close]'),
             modal = document.querySelector(".modal");
 
     let hideModal = ()=>{
+        modal.classList.add('hide');
         modal.classList.remove('show');
         document.body.style.overflow = '';
     };
 
     let openModal = ()=>{
         modal.classList.add('show');
+        modal.classList.remove('hide');
         document.body.style.overflow = 'hidden';
         clearInterval(modalTimerId);
     };
@@ -116,11 +117,9 @@ function setClock(selector,endTime){
     modalBtn.forEach((item)=>{
         item.addEventListener('click',openModal);
     }); 
-
-    modalClose.addEventListener('click', hideModal); 
     
     modal.addEventListener('click',(e)=>{
-        if(e.target === modal){
+        if(e.target === modal || e.target.getAttribute('data-close') == ""){
             hideModal();
         }
     });
@@ -131,7 +130,7 @@ function setClock(selector,endTime){
         }
     });
 
-    const modalTimerId = setTimeout(openModal,3000);
+    const modalTimerId = setTimeout(openModal,50000);
 
     function showModalByScroll(){
         if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight){
@@ -217,7 +216,7 @@ function setClock(selector,endTime){
 
 const forms = document.querySelectorAll('form');
 const message = {
-    loading: 'Загрузка',
+    loading: 'img/form/spinner.svg',
     success: 'Спасибо, скоро мы с вами свяжемся',
     failure: 'Что-то пошло не так'
 };
@@ -230,10 +229,10 @@ function postData(form){
     form.addEventListener('submit',(e)=>{
         e.preventDefault();
 
-        const statusMessage = document.createElement('div');
-        statusMessage.classList.add('status');
-        statusMessage.textContent = message.loading;
-        form.append(statusMessage);
+        const statusMessage = document.createElement('img');
+        statusMessage.src = message.loading;
+        statusMessage.style.cssText = 'display: block; margin: 0 auto;';
+        form.insertAdjacentElement('afterend', statusMessage);        
 
         const request = new XMLHttpRequest();
         request.open('POST', 'server.php');
@@ -252,20 +251,45 @@ function postData(form){
         request.send(json);
 
         request.addEventListener('load',()=>{
+            console.log(json);
                 if(request.status ===200){
                     console.log(request.response);
-                    statusMessage.textContent = message.success;
+                    showThanksModal(message.success);
                     form.reset();
-                    setTimeout(()=>{
-                        statusMessage.remove();
-                    },2000);
+                    statusMessage.remove();
+
                 }
                 else{
-                    statusMessage.textContent = message.failure;
+                    showThanksModal(message.failure);
                 }
         });
 
     });
+}
+
+/////////////MODAL AFTER HTTP REQUEST
+
+function showThanksModal(message){
+    const preveousModalDilog = document.querySelector(".modal__dialog");
+
+    preveousModalDilog.classList.add('hide');
+    openModal();
+
+    const thanksModal = document.createElement('div');
+    thanksModal.classList.add('modal__dialog');
+    thanksModal.innerHTML = `
+        <div class="modal__content">
+            <div class="modal__close" data-close>&times;</div>
+            <div class="modal__title">${message}</div>        
+        </div>
+    `;
+    document.querySelector('.modal').append(thanksModal);
+    setTimeout(()=>{
+        thanksModal.remove();
+        preveousModalDilog.classList.add('show');
+        preveousModalDilog.classList.remove('hide');
+        hideModal();
+    },4000);
 }
 
 
